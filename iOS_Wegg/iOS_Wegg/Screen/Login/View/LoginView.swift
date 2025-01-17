d06e35465d06008c910454901fd44e5df32f1173
KS <bubbleguy10@gmail.com>
2025-01-17 17:25:04 +0900

    feat: Asset에 Naver, Kakao 로그인 이미지 추가, LoginView 기본 구성

--- iOS_Wegg/iOS_Wegg/Screen/Login/View/LoginView.swift
+++ iOS_Wegg/iOS_Wegg/Screen/Login/View/LoginView.swift
@@ -7,14 +7,72 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class LoginView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Properties
+    
+    private let naverLoginButton = UIButton().then {
+        $0.setImage(UIImage(named: "Login/Naver/btnG_완성형"), for: .normal)
+    }
+    
+    private let kakaoLoginButton = UIButton().then {
+        $0.setImage(UIImage(named: "Login/Kakao/kakao_login_large_narrow"), for: .normal)
+    }
+    
+    private let signUpButton = UIButton().then {
+        $0.setTitle("회원가입", for: .normal)
+        $0.setTitleColor(.black , for: .normal)
+        $0.backgroundColor = .clear
+    }
+    
+    private let emailLoginButton = UIButton().then {
+        $0.setTitle("다른 방법으로 로그인", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.backgroundColor = .clear
+    }
+    
+    // MARK: - Setup
+    
+    private func setupViews() {
+        [naverLoginButton,
+         kakaoLoginButton,
+         signUpButton,
+         emailLoginButton].forEach { addSubview($0) }
+    }
+    
+    private func setupConstraints() {
+        naverLoginButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(64)
+            make.top.equalToSuperview().offset(363)
+        }
+        
+        kakaoLoginButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(64)
+            make.top.equalTo(naverLoginButton.snp.bottom).offset(24)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.leading.equalTo(kakaoLoginButton.snp.leading)
+            make.top.equalTo(kakaoLoginButton.snp.bottom).offset(24)
+        }
+        
+        emailLoginButton.snp.makeConstraints { make in
+            make.trailing.equalTo(kakaoLoginButton.snp.trailing)
+            make.top.equalTo(kakaoLoginButton.snp.bottom).offset(24)
+        }
+    }
 
 }
