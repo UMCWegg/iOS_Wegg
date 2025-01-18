2caecabe64102ec2a44ec318cd5c144c961d595d
KS <bubbleguy10@gmail.com>
2025-01-18 23:42:17 +0900

    refactor: 로그인에 사용하는 TextField Components 하위로 모듈화

---
+++ iOS_Wegg/iOS_Wegg/Component/TextField/Login/LoginTextField.swift
@@ -0,0 +1,50 @@
+//
+//  LoginTextField.swift
+//  iOS_Wegg
+//
+//  Created by 이건수 on 2025.01.18.
+//
+
+import UIKit
+
+class LoginTextField: UITextField {
+
+    // MARK: - Init
+    
+    init(placeholder: String, type: TextFieldType = .normal) {
+        super.init(frame: .zero)
+        self.placeholder = placeholder
+        setupTextField(type: type)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Types
+    
+    enum TextFieldType {
+        case normal
+        case email
+        case password
+    }
+    
+    // MARK: - Setup
+    
+    private func setupTextField(type: TextFieldType) {
+        backgroundColor = UIColor(named: "Login/TextField")
+        font = UIFont(name: "NotoSansKR-Regular", size: 13)
+        autocapitalizationType = .none
+        autocorrectionType = .no
+        
+        switch type {
+        case .normal:
+            break
+        case .email:
+            keyboardType = .emailAddress
+        case .password:
+            isSecureTextEntry = true
+        }
+    }
+
+}
