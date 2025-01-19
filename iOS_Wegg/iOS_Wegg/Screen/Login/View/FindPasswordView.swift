e3b9423f78fbac95fb90df53f97e9920d3930cd9
KS <bubbleguy10@gmail.com>
2025-01-19 12:31:03 +0900

    feat: FindPasswordView, ViewController 파일 추가

---
+++ iOS_Wegg/iOS_Wegg/Screen/Login/View/FindPasswordView.swift
@@ -0,0 +1,14 @@
+//
+//  FindPasswordView.swift
+//  iOS_Wegg
+//
+//  Created by 이건수 on 2025.01.19.
+//
+
+import UIKit
+
+class FindPasswordView: UIView {
+
+    
+
+}
---
+++ iOS_Wegg/iOS_Wegg/Screen/Login/ViewController/FindPasswordViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FindPasswordViewController.swift
+//  iOS_Wegg
+//
+//  Created by 이건수 on 2025.01.19.
+//
+
+import UIKit
+
+class FindPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
