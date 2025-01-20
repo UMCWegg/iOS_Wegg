f71f3db8dd73ac50352ade7bdeb3bbf7c218fac1
KS <bubbleguy10@gmail.com>
2025-01-20 16:57:28 +0900

    feat: View, ViewController 파일 추가.

---
+++ iOS_Wegg/iOS_Wegg/Screen/SignUp/View/ServiceAggrementView.swift
@@ -0,0 +1,20 @@
+//
+//  ServiceAggrementView.swift
+//  iOS_Wegg
+//
+//  Created by 이건수 on 2025.01.20.
+//
+
+import UIKit
+
+class ServiceAggrementView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ iOS_Wegg/iOS_Wegg/Screen/SignUp/ViewControllers/ServiceAggrementViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ServiceAggrementViewController.swift
+//  iOS_Wegg
+//
+//  Created by 이건수 on 2025.01.20.
+//
+
+import UIKit
+
+class ServiceAggrementViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    
+    override func loadView() {
+        <#code#>
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
