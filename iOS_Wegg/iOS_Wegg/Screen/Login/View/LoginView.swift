4958856355f0cc56e5f23b56ff0520b453d0d5b5
Lee KeonSoo <37922731+LeeKeonSoo@users.noreply.github.com>
2025-01-21 12:10:14 +0900

    feat: 소셜 로그인, 이메일 로그인 UI 구현 (#14)
    * feat: Asset에 Naver, Kakao 로그인 이미지 추가, LoginView 기본 구성
    
    * feat: 로그인 화면 구현
    
    * feat: 로그인 화면 구현
    
    * feat: 로그인 화면 구현
    
    * feat: 로그인 화면 구현
    
    * [feat] blue, yellow 색상 추가
    
    * Feat: 다른 방법으로 로그인 화면 구현
    
    * [bug] 로그인 버튼 글자 색상 정상화.
    
    * refactor: LoginButton. Component 하위 폴더로 구조 변경
    
    * style: MARK 순서 변경
    
    * refactor: Navigation 관련 로직 제거
    
    * Style: 내가 만든 blue, yellow 색상 제거
    
    * Fix: TextField 배경화면 안보이는 문제 해결
    
    * Style: 버튼 색 이름 Camel Case로 변경
    
    * refactor: 로그인에 사용하는 TextField Components 하위로 모듈화
    
    * refactor: EmailLoginView 안에 TextField. 커스텀 한 LoginTextField로 변경
    
    ---------
    
    Co-authored-by: KS <bubbleguy10@gmail.com>

--- iOS_Wegg/iOS_Wegg/Screen/Login/View/LoginView.swift
+++ iOS_Wegg/iOS_Wegg/Screen/Login/View/LoginView.swift
@@ -7,14 +7,97 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class LoginView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Properties
+    
+    private let appIcon = UIImageView().then {
+        $0.image = UIImage(named: "Login/wegg_icon")
+    }
+    
+    internal let naverLoginButton = LoginButton(
+        title: "네이버로 로그인",
+        backgroundColor: UIColor(hex: "03C75A") ?? .systemGreen
+    )
+        
+    internal let kakaoLoginButton = LoginButton(
+        title: "카카오로 로그인",
+        backgroundColor: UIColor(hex: "FEE500") ?? .systemYellow
+    )
+        
+    internal let emailLoginButton = LoginButton(
+        title: "다른 방법으로 로그인",
+        backgroundColor: UIColor(hex: "C7C7C7") ?? .systemGray
+    )
+
+    private let signUpButton = UIButton().then {
+        let attributedString = NSAttributedString(
+            string: "회원가입",
+            attributes: [
+                .underlineStyle: NSUnderlineStyle.single.rawValue,
+                .baselineOffset: 6,
+                .font: UIFont(name: "NotoSansKR-Regular", size: 16) ?? .systemFont(ofSize: 16)
+            ]
+        )
+        $0.setAttributedTitle(attributedString, for: .normal)
+    }
+    
+    // MARK: - Setup
+    
+    private func setupViews() {
+        [
+            appIcon,
+            naverLoginButton,
+            kakaoLoginButton,
+            emailLoginButton,
+            signUpButton
+        ].forEach { addSubview($0) }
+    }
+    
+    private func setupConstraints() {
+        
+        appIcon.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(322)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(54)
+        }
+        
+        emailLoginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(17)
+            make.bottom.equalTo(signUpButton.snp.top).offset(-16)
+        }
+        
+        kakaoLoginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.leading.trailing.equalTo(emailLoginButton)
+            make.bottom.equalTo(emailLoginButton.snp.top).offset(-15)
+        }
+        
+        naverLoginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.leading.trailing.equalTo(emailLoginButton)
+            make.bottom.equalTo(kakaoLoginButton.snp.top).offset(-15)
+        }
+    }
 
 }
--- iOS_Wegg/iOS_Wegg/Screen/Login/ViewController/LoginViewController.swift
+++ iOS_Wegg/iOS_Wegg/Screen/Login/ViewController/LoginViewController.swift
@@ -7,12 +7,43 @@
 
 import UIKit
 
+import Then
+
 class LoginViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let loginView = LoginView()
+    
+    // MARK: - Lifecycle
+    
+    override func loadView() {
+        view = loginView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        setupActions()
+        
+    }
+    
+    // MARK: - Setup
+    
+    private func setupActions() {
+        loginView.emailLoginButton.addTarget(
+            self,
+            action: #selector(emailLoginButtonTapped),
+            for: .touchUpInside)
+    }
+    
+    // Navigation 추후 구현
+    
+    // MARK: - Actions
+    
+    @objc private func emailLoginButtonTapped() {
+        let emailLoginVC = EmailLoginViewController()
+        navigationController?.pushViewController(emailLoginVC, animated: true)
     }
     
 }
