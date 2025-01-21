e46819d3b65c05d9cd703248757a82e74bbb80aa
Mr.Song <lssyl_@naver.com>
2025-01-21 15:17:28 +0900

    feat:메인 탭 바 구현 (#20)
    * feat: 둘러보기 뷰 기본 UI 구현
    
    * fix: 색상 및 폰트 업데이트
    
    * feat: 메인탭바개발 및 각 뷰 폴더링
    
    * feat: 탭바 아이템 클릭시 색상 적용
    
    * refactor: 탭바 컨트롤러에서 메인뷰들 이름 변경

---
+++ iOS_Wegg/iOS_Wegg/Screen/Time/TimeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookViewController.swift
+//  iOS_Wegg
+//
+//  Created by 송승윤 on 1/20/25.
+//
+
+import UIKit
+
+class TimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
