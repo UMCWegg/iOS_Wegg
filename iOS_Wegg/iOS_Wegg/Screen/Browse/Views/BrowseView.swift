bc01b59deca3d8fe9d8d1c89bc5877e0ab815857
Mr.Song <lssyl_@naver.com>
2025-01-21 16:22:41 +0900

    feat: 검색바 UI 개발  (#23)
    * feat: 둘러보기 뷰 기본 UI 구현
    
    * fix: 색상 및 폰트 업데이트
    
    * feat: 검색바 UI 개발
    
    ---------
    
    Co-authored-by: Jaewon Lee <50106190+jwon0523@users.noreply.github.com>

--- iOS_Wegg/iOS_Wegg/Screen/Browse/Views/BrowseView.swift
+++ iOS_Wegg/iOS_Wegg/Screen/Browse/Views/BrowseView.swift
@@ -14,6 +14,7 @@ class BrowseView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addcomponents()
         setupView()
         setupConstraints()
         self.backgroundColor = .white
@@ -35,6 +36,14 @@ class BrowseView: UIView {
         return collectionView
     }()
     
+    /// 검색바 헤더 뷰 추가
+    public lazy var searchView: SearchView = SearchView()
+    
+    // MARK: - Methods
+    
+    /// UI 구성 요소 추가
+    private func addcomponents() {
+        [browseCollectionView, searchView].forEach{ self.addSubview($0)}
     // MARK: - Methods
     
     /// UI 구성 요소 추가
@@ -44,10 +53,14 @@ class BrowseView: UIView {
     
     /// UI 제약 조건 설정
     private func setupConstraints() {
-        browseCollectionView.snp.makeConstraints { make in
-            make.edges.equalToSuperview().inset(
+        browseCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(
                 UIEdgeInsets(top: 120, left: 0, bottom: 50, right: 0)
             )
+        }
+        
+        searchView.snp.makeConstraints{
+            $0.top.left.right.equalToSuperview()
         }
     }
     
