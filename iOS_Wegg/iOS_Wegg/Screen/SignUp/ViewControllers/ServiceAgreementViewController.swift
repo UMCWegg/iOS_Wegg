e8306792d3d3f9b7d06ea6975da84d43dc6e30b9
KS <bubbleguy10@gmail.com>
2025-01-27 05:27:11 +0900

    feat: 서비스 이용 동의 화면 구현.

--- iOS_Wegg/iOS_Wegg/Screen/SignUp/ViewControllers/ServiceAgreementViewController.swift
+++ iOS_Wegg/iOS_Wegg/Screen/SignUp/ViewControllers/ServiceAgreementViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ServiceAggrementViewController.swift
+//  ServiceAgreementViewController.swift
 //  iOS_Wegg
 //
 //  Created by 이건수 on 2025.01.20.
@@ -7,19 +7,88 @@
 
 import UIKit
 
-class ServiceAggrementViewController: UIViewController {
+class ServiceAgreementViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    private let serviceAgreementView = ServiceAggrementView()
+    private var isMarketingAgreed: Bool = false
+    private lazy var toggleButtons: [SignUpToggleButton] = [
+        serviceAgreementView.serviceToggleButton,
+        serviceAgreementView.privacyToggleButton,
+        serviceAgreementView.locationToggleButton,
+        serviceAgreementView.marketingToggleButton
+    ]
+    
     // MARK: - Lifecycle
     
     override func loadView() {
-        <#code#>
+        view = serviceAgreementView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        view.backgroundColor = .white
+        setupButtons()
     }
     
-
+    // MARK: - Setup
+    
+    private func setupButtons() {
+        serviceAgreementView.agreeAllToggleButton.setOnToggleListener { [weak self] isChecked in
+            self?.handleAllAgreementToggle(isChecked)
+        }
+        
+        toggleButtons.forEach { button in
+            button.setOnToggleListener { [weak self] _ in
+                self?.checkAllAgreement()
+                self?.updateNextButtonState()
+            }
+        }
+        
+        serviceAgreementView.marketingToggleButton.setOnToggleListener { [weak self] isChecked in
+            self?.isMarketingAgreed = isChecked
+        }
+        
+        serviceAgreementView.nextButton.addTarget(
+            self,
+            action: #selector(nextButtonTapped),
+            for: .touchUpInside
+        )
+        
+        serviceAgreementView.backButton.addTarget(
+            self,
+            action: #selector(backButtonTapped),
+            for: .touchUpInside
+        )
+    }
+    
+    // MARK: - Functions
+    
+    private func handleAllAgreementToggle(_ isChecked: Bool) {
+        toggleButtons.forEach { $0.setChecked(isChecked) }
+        updateNextButtonState()
+    }
+    
+    private func checkAllAgreement() {
+        let allChecked = toggleButtons.allSatisfy { $0.getChecked() }
+        serviceAgreementView.agreeAllToggleButton.setChecked(allChecked)
+    }
+    
+    private func updateNextButtonState() {
+        let requiredButtons = toggleButtons[0...2]
+        let isEnabled = requiredButtons.allSatisfy { $0.getChecked() }
+        
+        serviceAgreementView.nextButton.alpha = isEnabled ? 1.0 : 0.3
+        serviceAgreementView.nextButton.isEnabled = isEnabled
+    }
+    
+    @objc private func nextButtonTapped() {
+        //let phoneNumVC = PhoneNumberViewController()
+        //navigationController?.pushViewController(phoneNumVC, animated: true)
+    }
+    
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
 }
