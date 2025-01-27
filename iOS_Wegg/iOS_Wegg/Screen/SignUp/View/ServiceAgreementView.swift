52f94198d899d25cada002ee844838aed634648d
Lee KeonSoo <37922731+LeeKeonSoo@users.noreply.github.com>
2025-01-28 02:03:46 +0900

    feat: 서비스 이용 동의 화면 구현 (#49)
    * feat: CheckButton 모듈화해서 구현
    
    * feat: ServiceAgreementView 중간 저장. API 구현으로 우선순위 변경.
    
    * feat: 사용자 이용 동의에 사용할 Custom ToggleButton 구현
    
    * feat: 서비스 이용 동의 화면 구현.
    
    * style: 누락된 타이틀 텍스트 추가
    
    * fix: 약관 전체 동의가 같이 체크되지 않는 버그 해결
    
    ---------
    
    Co-authored-by: KS <bubbleguy10@gmail.com>

--- iOS_Wegg/iOS_Wegg/Screen/SignUp/View/ServiceAgreementView.swift
+++ iOS_Wegg/iOS_Wegg/Screen/SignUp/View/ServiceAgreementView.swift
@@ -11,12 +11,14 @@ class ServiceAggrementView: UIView {
 
     // MARK: - Font
     
-    private let font = UIFont(name: "NotoSansKR-Regular", size: 15)
+    private let textFont = UIFont.notoSans(.regular, size: 15)
     
     // MARK: - Init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupViews()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -25,12 +27,95 @@ class ServiceAggrementView: UIView {
     
     // MARK: - Properties
     
-    private let serviceAgreementLabel = UILabel().then {
-        $0.text = "서비스 이용 동의"
-        $0.textColor = .black
-        $0.font = UIFont(name: "NotoSansKR-Regular", size: 20)
+    let backButton = UIButton().then {
+        $0.setImage(UIImage(named: "Login/BackButton"), for: .normal)
     }
     
+    private let mainLabel = UILabel().then {
+        $0.text = "서비스 이용 동의를 진행해주세요"
+        $0.textColor = .black
+        $0.font = UIFont.LoginFont.title
+    }
     
-
+    private let underLineView = UIView().then {
+        $0.backgroundColor = UIColor.LoginColor.labelColor
+        $0.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    let agreeAllToggleButton = SignUpToggleButton(text: "약관 전체 동의")
+    let serviceToggleButton = SignUpToggleButton(text: "(필수) 서비스 이용약관")
+    let privacyToggleButton = SignUpToggleButton(text: "(필수) 개인정보 수집/이용 동의")
+    let locationToggleButton = SignUpToggleButton(text: "(필수) 위치정보 제공")
+    let marketingToggleButton = SignUpToggleButton(text: "(선택) 마케팅 수신 동의")
+    
+    let nextButton = LoginButton(
+        style: .textOnly,
+        title: "다음",
+        backgroundColor: UIColor.customColor(.secondary)
+    )
+    
+    // MARK: - Setup
+    
+    private func setupViews() {
+        [
+            backButton,
+            mainLabel,
+            underLineView,
+            agreeAllToggleButton,
+            serviceToggleButton,
+            privacyToggleButton,
+            locationToggleButton,
+            marketingToggleButton,
+            nextButton
+        ].forEach { addSubview($0) }
+    }
+    
+    private func setupConstraints() {
+        backButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(70)
+            make.leading.equalToSuperview().offset(17)
+            make.width.equalTo(8)
+        }
+        
+        mainLabel.snp.makeConstraints { make in
+            make.top.equalTo(backButton.snp.bottom).offset(22)
+            make.leading.equalTo(backButton)
+        }
+        
+        agreeAllToggleButton.snp.makeConstraints { make in
+            make.leading.equalTo(backButton)
+            make.top.equalTo(mainLabel.snp.bottom).offset(85)
+        }
+        
+        underLineView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(backButton)
+            make.top.equalTo(agreeAllToggleButton.snp.bottom).offset(22)
+        }
+        
+        serviceToggleButton.snp.makeConstraints { make in
+            make.leading.equalTo(backButton)
+            make.top.equalTo(underLineView.snp.bottom).offset(22)
+        }
+        
+        privacyToggleButton.snp.makeConstraints { make in
+            make.leading.equalTo(backButton)
+            make.top.equalTo(serviceToggleButton.snp.bottom).offset(12)
+        }
+        
+        locationToggleButton.snp.makeConstraints { make in
+            make.leading.equalTo(backButton)
+            make.top.equalTo(privacyToggleButton.snp.bottom).offset(12)
+        }
+        
+        marketingToggleButton.snp.makeConstraints { make in
+            make.leading.equalTo(backButton)
+            make.top.equalTo(locationToggleButton.snp.bottom).offset(12)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-40)
+        }
+    }
 }
