44a7e372bc3702a39d7574caae6d3d9dabf1b380
KS <bubbleguy10@gmail.com>
2025-01-28 00:51:32 +0900

    feat: 이메일 회원가입 파일 생성. 초기 구성.

---
+++ iOS_Wegg/iOS_Wegg/Screen/SignUp/ViewControllers/EmailSignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmailSignUpViewController.swift
+//  iOS_Wegg
+//
+//  Created by 이건수 on 2025.01.27.
+//
+
+import UIKit
+
+class EmailSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
