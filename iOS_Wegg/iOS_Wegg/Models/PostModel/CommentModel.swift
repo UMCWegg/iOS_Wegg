5d620b2d35b70aabc7cb5800462912b1a0671794
SongCodeMaster <lssyl_@naver.com>
2025-02-01 05:29:02 +0900

    feat: 댓글창 Model 초기 설정 및 CommnetView 개발중

---
+++ iOS_Wegg/iOS_Wegg/Models/PostModel/CommentModel.swift
@@ -0,0 +1,14 @@
+//
+//  CommentModel.swift
+//  iOS_Wegg
+//
+//  Created by 송승윤 on 2/1/25.
+//
+
+import Foundation
+
+/// 댓글 데이터를 관리하는 모델
+struct CommentModel {
+    let userName: String // 댓글 작성자 닉네임
+    let content: String // 댓글 내용
+}
---
+++ iOS_Wegg/iOS_Wegg/Screen/Browse/PostDetail/Views/CommentView.swift
@@ -0,0 +1,19 @@
+//
+//  CommentView.swift
+//  iOS_Wegg
+//
+//  Created by 송승윤 on 2/1/25.
+//
+
+import UIKit
+
+class CommentView: UIView {
+    
+    // MARK: - Init
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Properties
+    
+}
