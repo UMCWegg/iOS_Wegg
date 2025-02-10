c7c277ab5be548063f97a131b7b33db6e1391f50
jwon0523 <jwon010523@gmail.com>
2025-02-10 11:11:45 +0900

    AddScehduleView 생성

---
+++ iOS_Wegg/iOS_Wegg/Screen/Schedule/AddSchedule/View/AddScheduleView.swift
@@ -0,0 +1,21 @@
+//
+//  AddScheduleView.swift
+//  iOS_Wegg
+//
+//  Created by jaewon Lee on 2/10/25.
+//
+
+import UIKit
+
+class AddScheduleView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellowWhite
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
