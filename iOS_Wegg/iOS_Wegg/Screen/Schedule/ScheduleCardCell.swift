036de822468d61abc688efe5fce20c87ac78bba7
Jaewon Lee <50106190+jwon0523@users.noreply.github.com>
2025-02-10 19:35:01 +0900

    feat: 일정 페이지 UI 제작 (#105)
    * refactor: TimeViewController -> ScheduleViewController로 이름 변경
    
    * feat: 헤더 바 생성 완료
    
    * feat: UITableView 초기 설정 완료
    
    * feat: ScheduleCardCell UI 완료
    
    * 셀 선택 효과 제거
    
    * docs: 주석 처리
    
    * fix: 13mini 레이아웃 수정

---
+++ iOS_Wegg/iOS_Wegg/Screen/Schedule/ScheduleCardCell.swift
@@ -0,0 +1,125 @@
+//
+//  ScheduleCardCell.swift
+//  iOS_Wegg
+//
+//  Created by jaewon Lee on 2/10/25.
+//
+
+import UIKit
+import Then
+
+class ScheduleCardCell: UITableViewCell {
+    static let reuseIdentifier = "ScheduleCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .yellowWhite
+        contentView.backgroundColor = .clear
+        selectionStyle = .none // 셀 선택 효과 제거
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Property
+    
+    private lazy var containerView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 24
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.secondary.cgColor
+        $0.layer.masksToBounds = true
+    }
+    
+    private lazy var yellowLogoIcon = UIImageView().then {
+        $0.image = UIImage(named: "yellow_wegg_icon")
+        $0.contentMode = .scaleAspectFit
+    }
+    private lazy var dateLabel = UILabel().then {
+        $0.font = .notoSans(.medium, size: 14)
+        $0.textColor = .black
+    }
+    private lazy var titleLabel = UILabel().then {
+        $0.font = .notoSans(.bold, size: 20)
+        $0.textColor = .black
+    }
+    private lazy var timeRangeLabel = UILabel().then {
+        $0.font = .notoSans(.medium, size: 16)
+        $0.textColor = .customGray
+    }
+    private lazy var toggleSwitch = UISwitch().then {
+        $0.onTintColor = .primary
+    }
+    
+    private lazy var logoTitleStack = UIStackView().then {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.spacing = 4
+    }
+    
+    // 테이블 datasoruce 주입
+    func configure(with schedule: Schedule) {
+        dateLabel.text = schedule.date
+        titleLabel.text = schedule.location
+        timeRangeLabel.text = schedule.timeRange
+        toggleSwitch.isOn = schedule.isOn
+    }
+}
+
+// MARK: - Set Up Extension
+
+private extension ScheduleCardCell {
+    func setupView() {
+        addComponents()
+        constraints()
+    }
+    
+    func addComponents() {
+        contentView.addSubview(containerView)
+        [yellowLogoIcon, titleLabel].forEach(logoTitleStack.addArrangedSubview)
+        [
+            dateLabel,
+            logoTitleStack,
+            timeRangeLabel,
+            toggleSwitch
+        ].forEach(containerView.addSubview)
+        
+        yellowLogoIcon.snp.makeConstraints { make in
+            make.width.height.equalTo(16)
+        }
+    }
+    
+    func constraints() {
+        containerView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(19)
+            make.leading.trailing.equalToSuperview().inset(21)
+            make.bottom.equalToSuperview()
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.lessThanOrEqualToSuperview().offset(20)
+            make.width.equalTo(160)
+        }
+        
+        logoTitleStack.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp.bottom).offset(5)
+            make.leading.lessThanOrEqualToSuperview().offset(20)
+            make.width.equalTo(230)
+            make.height.equalTo(26)
+        }
+        
+        timeRangeLabel.snp.makeConstraints { make in
+            make.top.equalTo(logoTitleStack.snp.bottom).offset(5)
+            make.leading.lessThanOrEqualToSuperview().offset(20)
+            make.width.equalTo(160)
+        }
+        
+        toggleSwitch.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(44)
+            make.trailing.lessThanOrEqualToSuperview().offset(-20)
+        }
+    }
+}
---
+++ iOS_Wegg/iOS_Wegg/Screen/Schedule/ScheduleViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ScheduleViewController.swift
+//  iOS_Wegg
+//
+//  Created by jaewon Lee on 1/20/25.
+//
+
+import UIKit
+
+struct Schedule: Hashable {
+    let id: UUID
+    let date: String
+    let location: String
+    let timeRange: String
+    var isOn: Bool
+}
+
+class ScheduleViewController: UIViewController {
+    
+    // UITableViewDiffableDataSource를 사용하여 데이터 관리
+    private var dataSource: UITableViewDiffableDataSource<Int, Schedule>?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view = scheduleView
+        setupDataSource()
+        applyInitialSnapshot()
+    }
+
+    lazy var scheduleView = ScheduleView().then {
+        $0.studyCardTableView.delegate = self
+    }
+
+    private func setupDataSource() {
+        // Diffable Data Source를 생성하여 테이블 뷰에 연결
+        dataSource = UITableViewDiffableDataSource<Int, Schedule>(
+            tableView: scheduleView.studyCardTableView
+        ) { tableView, indexPath, schedule in
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: ScheduleCardCell.reuseIdentifier,
+                for: indexPath
+            ) as? ScheduleCardCell else {
+                return UITableViewCell()
+            }
+            
+            // 셀에 데이터 구성
+            cell.configure(with: schedule)
+            return cell
+        }
+    }
+
+    private func applyInitialSnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Schedule>()
+        snapshot.appendSections([0]) // 단일 섹션 추가
+        snapshot.appendItems([
+            Schedule(
+                id: UUID(),
+                date: "1월 14일",
+                location: "스타벅스 미아점",
+                timeRange: "9:00 AM ~ 12:00 PM",
+                isOn: true
+            ),
+            Schedule(
+                id: UUID(),
+                date: "1월 15일",
+                location: "투썸플레이스 강남점",
+                timeRange: "10:00 AM ~ 1:00 PM",
+                isOn: false
+            )
+        ])
+        // DataSource가 nil이 아닌 경우 스냅샷 적용
+        guard let dataSource = dataSource else { return }
+        dataSource.apply(snapshot, animatingDifferences: true)
+        
+//        // 현재 스냅샷 가져오기
+//        let currentSnapshot = dataSource.snapshot()
+//        // 스냅샷에서 모든 아이템 가져오기
+//        print(currentSnapshot.itemIdentifiers)
+    }
+}
+
+extension ScheduleViewController: UITableViewDelegate {
+    /*
+    // 셀 선택 시 호출되는 메서드
+    // 현재 주석 처리: 필요 시 활성화
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        guard let dataSource = dataSource else { return }
+        guard let selectedItem = dataSource.itemIdentifier(
+            for: indexPath
+        ) else { return }
+        print("선택된 항목: \(selectedItem.location)")
+    }
+    */
+    
+    // 각 셀의 높이를 설정하는 메서드
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 138 // 셀 높이를 138로 고정
+    }
+}
